bcd07dad6de6352b9544f6b00b8caecd936da240
Shaun DeVos <mk7upurz87@gmail.com>
2013-04-25 18:31:28 -0400

    ...fixed sort of, started tile formatting.

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,3 +1,6 @@
-﻿$(function() {
-
+﻿$(function() {  
+    var $tile = $('.tile');
+    $tile.each(function(e){
+        e.find('h3').first().after('<button class="tile-minimize">_</button>');
+    })
 })
--- Scripts/jquery.neosmart.fb.wall.js
+++ Scripts/jquery.neosmart.fb.wall.js
@@ -273,4 +273,4 @@
 		accessToken:			''
 	};
 
-})(jQuery);
+})($);
