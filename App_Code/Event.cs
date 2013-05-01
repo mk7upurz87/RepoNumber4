92e5d6160a0cb653ba2043ed4cd41e0c42c581ae
Shannon Trudeau <smt9020@rit.edu>
2013-05-01 18:55:58 -0400

    working with the database with a c sharp file

---
+++ App_Code/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Need this to work with the sql database and return a list of all the events in the database
+/// </summary>
+public class Event
+{
+	public Event()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
