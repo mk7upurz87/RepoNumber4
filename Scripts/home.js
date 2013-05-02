767d737b87984c7d61dda17709271b9b0c296a18
Shaun DeVos <mk7upurz87@gmail.com>
2013-05-02 17:50:23 -0400

    more styling stuff

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,13 +1,45 @@
 ﻿$(function () {
     var $tile = $('div.tile');
-    $tile.append('<div><button class="tile-minimize"><p>-</p></button></div>');
-    console.log($tile);
+    $tile.append('<div class="btn-minimize tile-minimize">-</div>');
 
-    var minimizedTiles = [];
+    var modifiedTiles = {};
 
-    $('button.tile-minimize').click(function () {
-        $(this).parent().slideUp(function () {
-            minimizedTiles.add($(this));
-        });
-    });
+    /***********************************************************
+     * Minimize the tile that the minimize button resides on,
+     * add that tile to a map of minimized tiles and pair it to
+     * it's original height
+     **********************************************************/
+    var minimize = function (e) {
+        e.preventDefault();
+
+        var $tile = $(this).parents('.tile');
+        modifiedTiles[$tile.id] = $tile.attr("height");
+
+        var $sibling = $tile.siblings('.tile');
+        modifiedTiles[$sibling.id] = $sibling.attr("height");
+
+        $tile.animate({
+            height: '15px'
+        }).siblings('.tile').animate({
+            height: '910px'
+        })
+    }
+
+    /************************************************************
+     * Restore the tile that the minimize button resides by asking
+     * the map for the tiles id and reassigning that tiles original
+     * height
+     ************************************************************/
+    var restore = function(e) {        
+        e.preventDefault();
+        var $tile = $(e.target).parents('.tile');
+
+        $tile.animate({
+            height: modifiedTiles[this.id],
+        }).siblings('.tile').animate({
+            height: modifiedTiles[this.id]
+        })
+    };
+
+    $('.tile-minimize').click(minimize);
 })
