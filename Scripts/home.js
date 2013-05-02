b3c7773e7138783be30833fee11efffd9c4bc7f5
Shaun DeVos <mk7upurz87@gmail.com>
2013-05-01 23:47:50 -0400

    now bear with me here.  I got the main layout working!  However, stocks isn't work again.  I tried to fix it for a while but it still isnt.  You'll notice some div name changes and some id's changed because when merging layouts, the styling was targetting unintended elements.  Hope this leads you to a good start point for troubleshooting.  Shouldn't be too many large changes like this again.

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,4 +1,13 @@
-﻿$(function() {  
-    var $tile = $('.tile');
-    $tile.append('<button class="tile-minimize">_</button>');
+﻿$(function () {
+    var $tile = $('div.tile');
+    $tile.append('<div>click my minimize button  --&gt<button class="tile-minimize"><b>_</b></button></div>');
+    console.log($tile);
+
+    var minimizedTiles = [];
+
+    $('button.tile-minimize').click(function () {
+        $(this).parent().slideUp(function () {
+            minimizedTiles.add($(this));
+        });
+    });
 })
