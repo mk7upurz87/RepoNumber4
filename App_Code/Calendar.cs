0a5d218cf866bd6d478a5f7f66f5440ae8b0bd16
Shannon Trudeau <smt9020@rit.edu>
2013-05-04 11:29:16 -0400

    calendar with database woohoo

--- App_Code/Calendar.cs
+++ App_Code/Calendar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 /// <summary>
 /// Summary description for Calendar
@@ -9,18 +10,49 @@ using System.Web;
 public class Calendar
 {
     List<string> events;
+    
+    SqlConnection myConnection = new SqlConnection("user id=webuser;" +
+                                       "password=webuser;server=SHANNON-CHARLES;" +
+                                       "Trusted_Connection=True;" +
+                                       "database=WebEngineering; " +
+                                       "connection timeout=30");
 	public Calendar()
 	{
         events = new List<string>();
-        string hey = "hey";
-        events.Add(hey + "1");
-        events.Add(hey + "2");
-        events.Add(hey + "3");
 	}
 
-    public string getNames(int i)
+    public string database()
     {
-        return events.GetRange(i-1,i).ToString();
+        try
+        { myConnection.Open(); }
+        catch (Exception e)
+        { Console.WriteLine(e.ToString()); }
+
+        try
+        {
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand("select * from calendar",
+                                                     myConnection);
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+
+                return myReader["name"].ToString();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        return "whoops!";
     }
-    
+
+    public List<string> getNames()
+    {
+        System.Diagnostics.Debug.Write(events.ToString());
+        return events;
+    }
+
+
+
 }
