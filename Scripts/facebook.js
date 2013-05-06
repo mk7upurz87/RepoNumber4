b5e075d1660935571be9addf05b56c511d5c032d
Adam Blaine <amb8805@rit.edu>
2013-05-06 17:47:26 -0400

    adihsaoidhsaod

--- Scripts/facebook.js
+++ Scripts/facebook.js
@@ -12,9 +12,7 @@ window.fbAsyncInit = function () {
 
     FB.getLoginStatus(function (response) {
         if (response.status === 'connected') {
-            userID = response.userID; source = 'http://graph.facebook.com/' + userID + '/picture';
-            document.getElementById(profile).src = source;
-            email = response.email;
+           
             $('#social').fbWall({
                 id: response.authResponse.userID,
                 accessToken: response.authResponse.accessToken,
@@ -38,9 +36,7 @@ window.fbAsyncInit = function () {
                 });
             
 
-            });
-                  
-               
+            })
             $('a#register').hide();
             document.getElementById('content').style.display = "inline";
             // connected
