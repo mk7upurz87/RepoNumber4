01c21f6524f3bfb7ca542837b35a1abb9abee059
Adam Blaine <amb8805@rit.edu>
2013-05-07 03:49:28 -0400

    yay conflicts gone

--- Scripts/facebook.js
+++ Scripts/facebook.js
@@ -13,36 +13,26 @@ window.fbAsyncInit = function () {
 
     FB.getLoginStatus(function (response) {
         if (response.status === 'connected') {
-            $('a#register').hide();
-            $('a#loginbutton').text('Log out');
-            $('a#loginbutton').click(function () {
-                if ($(this).text() == ("Log out")) {
-                    FB.logout();
-                }
-                console.log($(this).text());
-                document.getElementById('feed-content').style.display = "inline";
-                // connected
-            });
-
+            
             var myurl = 'https://graph.facebook.com/me/home?access_token=' + response.authResponse.accessToken;
             var rawjson;
-
+         
             $.ajax({
                 url: myurl,
                 type: 'GET',
                 dataType: "jsonp",
-                success: function (json) {
+                success: function(json){
                     rawjson = json;
-                    $.each(rawjson.data, function (index, dataObj) {
+                    $.each(rawjson.data, function (index,dataObj) {
                         if (dataObj.message) {
-                            var profimgs = $('<img class="friendimg" src="http://graph.facebook.com/' + dataObj.from.id + '/picture?type=normal" />'); // object which I will append the list items into
-                            var name = $('<p  class="fbcontent fbname"><b>' + dataObj.from.name + '</b></p>'); // object which I will append the list items into
+                            var profimgs = $('<img class="friendimg" src="http://graph.facebook.com/' + dataObj.from.id + '/picture?type=square" />'); // object which I will append the list items into
+                            var name = $('<p  class="fbcontent fbname"><b>'+ dataObj.from.name +'</b></p>'); // object which I will append the list items into
                             var msg = $('<p class="fbcontent">   ' + dataObj.message + '</p></br>');
                             $('#wall').append(profimgs.clone());
                             $('#wall').append(name.clone());
                             $('#wall').append(msg.clone());
                             if (dataObj.picture) {
-                                var msgimg = $('<img class="fbmsgimg" src="' + dataObj.picture + '" />');
+                                var msgimg = $('<img class="fbmsgimg" src="'+ dataObj.picture + '" />');
                                 $('#wall').append(msgimg.clone());
                             }
                             else if (dataObj.likes) {
@@ -54,42 +44,71 @@ window.fbAsyncInit = function () {
                                 }
                                 $('#wall').append(likes.clone());
                             }
+                           // else if (dataObj.comments) {
+                            //    $.each(dataObj.comments.data, function (index, commentObj) {
+                                //    var comment = $('<p class="comment">' + commentObj.from.name + '</p><p class="comment commentmsg>' + commentObj.message + '</p>');
+                                 //   $('#wall').append(comment.clone());
+                                 //   console.log(commentObj);
+                               // });
+                         //   }
+                           // else if (dataObj.link) {
+                           //     var link = $('<a href="' + dataObj.link + '">' + dataObj.name + '</a>');
+                            //    $('#wall').append(link.clone());
+                           // }
+                       
                         }
                     });
                     console.log(json);
                 }
             });
-            /* $('#social').fbWall({
-                 id: response.authResponse.userID,
-                 accessToken: response.authResponse.accessToken,
-                 showGuestEntries: true,
-                 showComments: true,
-                 max: 5,
-                 timeConversion: 12
-                    
-             });
-         */
-            FB.api('/me', function (response) {
-                $.loadCalendar(response);
-                setProfileImage(response);
+           
+
+
+            FB.api('/me', function (meResponse) {
+                $('#fb-Hidden-Content').attr('value', (meResponse.id));
+
+                // Set the profile image in the profile tile
+                setProfileImage(response, meResponse);
+
             });
-                FB.api('/me', function (meResponse) {
-                    $('#fb-Hidden-Content').attr('value', (meResponse.id));
 
-                    // Set the profile image in the profile tile
-                    setProfileImage(response, meResponse);
+            $('#social').fbWall({
+                id: response.authResponse.userID,
+                accessToken: response.authResponse.accessToken,
+                showGuestEntries: true,
+                showComments: true,
+                max: 5,
+                timeConversion: 12
+                   
+            });
+            $('a#loginbutton').text('Log out');
+            $(function () {
+                $('a#loginbutton').click(function () {
+                    if ($(this).text() == ("Log out")) {
+                               
+                        FB.logout();
+                    }
+                    else {
+                              
+                    }
+                    console.log($(this).text());
+                });
+            
 
+            })
+            $('a#register').hide();
+            document.getElementById('feed-content').style.display = "inline";
+            // connected
+        } else if (response.status === 'not_authorized') {
+            // not_authorized
+               
+            // $('#social).hide();
                 
-            });
-        }
-
-
-
-        else {
+        } else {
             console.log("i got here");
             $('#social').hide();
             $('form').hide();
-
+             
             // not_logged_in
         }
     });
@@ -103,11 +122,11 @@ window.fbAsyncInit = function () {
         '//connect.facebook.net/en_US/all.js';
     e.async = true;
     document.getElementById('fb-root').appendChild(e);
-});
+}());
     
 function setStatus() {
        
-    status1 = document.getElementById('status').value;
+    status1 = document.getElementById('fb-status').value;
     FB.api(
     {
         method: 'status.set',
@@ -128,14 +147,14 @@ function uploadphoto() {
     var imgsrc = document.getElementById("uploadImage").src;
     var wallPost = {
         message: $('#status').val(),
-      
+        imgUrl: imgsrc
     };
     console.log(imgsrc);
     FB.api('/me/feed', 'post', wallPost, function (response) {
         if (!response || response.error) {
-            alert('Error occured');
+            
         } else {
-            alert('Post ID: ' + response);
+            
         }
     });
     var imgURL = $('#uploadImage').attr('src');
@@ -160,15 +179,15 @@ function uploadphoto() {
 
 $('#postbut').click(function() {
     console.log("posting status");
-    setStatus();
+    uploadphoto();
 });
 
 $('#showpic').click(function() {
     uploadphoto();
 });
 
+
 function setProfileImage(user, meUser) {
     $("#profile-content").append("<img src='" + 'http://graph.facebook.com/' + user.authResponse.userID + '/picture?type=large' + "'></img>");
     $("#profile-content").append("<h4 id='quotes-div'>" + meUser.quotes + "</h4>");
 }
-
